f72e5463acb5a7afe2b251456f56088d1415919e
theTisch21 <samtisch@live.com>
2021-06-12 21:29:44 +0000

    Fix for github pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const fs = require('fs');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.set('Content-Type', 'text/html');
+  res.send(Buffer.from(fs.readFileSync(path.join(__dirname, '../docs/index.html'))));
 });
 
 module.exports = router;
